68d9b4e7c04721b4d8fd2b0f18e2225e191718f1
Jack Tierney <jackcurragh@gmail.com>
2025-03-10 06:51:48 +0000

    rename directory

--- Translonpredictor/__init__.py
+++
@@ -1,8 +0,0 @@
-"""TranslonScorer: A tool for identifying and scoring translational events from Ribo-seq data."""
-
-from .cli import cli
-
-__author__ = """Jack Tierney"""
-__email__ = "jackcurragh@gmail.com"
-__version__ = "0.1.0"
-__all__ = ['cli']
